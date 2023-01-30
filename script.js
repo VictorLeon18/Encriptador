95ee21f1e921fe2dcf3c250f65c64c584bc63eb5
Victor León <vic18.leon@gmail.com>
2023-01-29 23:18:20 -0500

    Actualización del Encriptador

---
+++ script.js
@@ -0,0 +1,54 @@
+const textArea = document.querySelector(".texto");
+const mensaje = document.querySelector(".rectangulo");
+const titulo = document.querySelector(".mensaje1");
+const subtitulo = document.querySelector(".mensaje2");
+
+function btnEncriptar(){
+    const textoEncriptado = encriptar(textArea.value);
+    mensaje.value = textoEncriptado;
+    textArea.value = "";
+    mensaje.style.backgroundImage = "none";
+    titulo.style.color= "transparent";
+    subtitulo.style.color= "transparent";
+}
+
+function btnDesencriptar(){
+    const textoDesencriptado = desencriptar(textArea.value);
+    mensaje.value = textoDesencriptado;
+    textArea.value = "";
+    mensaje.style.backgroundImage = "none";
+    titulo.style.color= "transparent";
+    subtitulo.style.color= "transparent";
+}
+
+function btnCopiar(){
+    textArea.value = mensaje.value;
+    mensaje.value = "";
+    titulo.style.color= "white";
+    subtitulo.style.color= "white";
+}
+
+function encriptar(stringEncriptada){
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    stringEncriptada = stringEncriptada.toLowerCase();
+
+    for(let i = 0; i < matrizCodigo.length; i++){
+        if (stringEncriptada.includes(matrizCodigo[i][0])) {
+            stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1]);           
+        }
+    }
+    return stringEncriptada;
+}
+
+function desencriptar(stringDesencriptada){
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    stringDesencriptada = stringDesencriptada.toLowerCase();
+
+    for(let i = 0; i < matrizCodigo.length; i++){
+        if (stringDesencriptada.includes(matrizCodigo[i][1])) {
+            stringDesencriptada = stringDesencriptada.replaceAll(matrizCodigo[i][1], matrizCodigo[i][0]);           
+        }
+    }
+    return stringDesencriptada;
+}
+
